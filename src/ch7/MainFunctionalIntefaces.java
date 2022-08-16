5e39a777d051267603c2e5fb1beeb551d6cf445a
Nikilangelo4 <nlaptinskiy4@gmail.com>
2022-08-16 18:45:37 +0300

    add solution

--- src/ch7/MainFunctionalIntefaces.java
+++ src/ch7/MainFunctionalIntefaces.java
@@ -2,11 +2,17 @@ package ch7;
 
 public class MainFunctionalIntefaces {
     public static void main(String[] args) {
+
         ToBooleanFunctionInt toBooleanFunctionInt = (a) -> a % 13 == 0;
         BiFunctionString biFunctionString = (string1,string2) -> string2;
+        ToDoubleFunctionDouble toDoubleFunctionDouble = (a,b,c) -> b*b - 4*a*c;
+
         System.out.println(biFunctionString.apply("12345","123456789"));
+
         System.out.println(toBooleanFunctionInt.apply(13));
         System.out.println(toBooleanFunctionInt.apply(14));
         System.out.println(toBooleanFunctionInt.apply(26));
+
+        System.out.println(toDoubleFunctionDouble.apply(2,3,4));
     }
 }
---
+++ src/ch7/ToDoubleFunctionDouble.java
@@ -0,0 +1,6 @@
+package ch7;
+
+@FunctionalInterface
+public interface ToDoubleFunctionDouble {
+    double apply(double a, double b, double c);
+}
